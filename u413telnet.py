ed5e0d388236267b430967c059755f922303862a
JamezQ <jamezmcclain@gmail.com>
2011-01-27 00:52:29 -0800

    Allowed to not crash on bad commands.

--- u413telnet.py
+++ u413telnet.py
@@ -6,7 +6,7 @@ import json
 import html2text
 import socket
 HOST = ''
-PORT = 9001
+PORT = 9000
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
@@ -18,31 +18,28 @@ urllib2.install_opener( o )
 
 def Term():
 	while True:
-		i = 2
-		conn.send("U413 > ")
-		a = conn.recv(1024)[:-2]
-		if a == "exit":
-			break
-		p = urllib.urlencode( { 'CommandString': a } )
-		f = o.open( 'http://u413.com/Terminal/ExecuteCommand', p)
-
-		data = f.read()
-		f.close()
-
-		data = json.loads(data)
-		if "reply" in a:
-			i=0
-		elif "delete" in a:
-			i=0
-		elif "logout" in a:
-			i=0
-		data = data['DisplayArray'][i]['Text']
-		data = data.encode("ASCII",'replace')
-		soup = BeautifulSoup(data)
-
-		Start.newhtml = html2text.html2text(str(soup))
-		conn.send("\n")
-		conn.send( Start.newhtml.replace("\n\n>","_"*76).replace("}\n\n","} ").encode("ASCII",'replace'))
+		try:
+			conn.send("U413 > ")
+			a = conn.recv(1024)[:-2]
+			if a == "exit":
+				break
+			p = urllib.urlencode( { 'CommandString': a } )
+			f = o.open( 'http://u413.com/Terminal/ExecuteCommand', p)
+			
+			data = f.read()
+			f.close()
+			
+			data = json.loads(data)
+			for i in range(4):
+				data2 = data['DisplayArray'][i]['Text']
+				data2 = data2.encode("ASCII",'replace')
+				soup = BeautifulSoup(data2)
+				
+				Start.newhtml = html2text.html2text(str(soup))
+				conn.send("\n")
+				conn.send( Start.newhtml.replace("\n\n>","_"*76).replace("}\n\n","} ").encode("ASCII",'replace'))
+		except:
+			None
 		if "logged out" in Start.newhtml:
 			Start()
 def Start():
